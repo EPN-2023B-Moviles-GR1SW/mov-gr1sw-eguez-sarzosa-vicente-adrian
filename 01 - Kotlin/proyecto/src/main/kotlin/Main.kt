49644fde63b0d9ce17273e9e86f293d17a1195d5
Adrian Eguez <eadepto@hotmail.com>
2023-11-21 16:02:08 -0500

    hasta reduce

--- 01 - Kotlin/proyecto/src/main/kotlin/Main.kt
+++ 01 - Kotlin/proyecto/src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 import java.util.*
 
-fun main(){
+fun main() {
     println("Hola mundo");
     // Tipos de variables
     // INMUTABLES (NO se reasignan "=")
@@ -10,12 +10,6 @@ fun main(){
     // Mutables (Re asignar)
     var mutable: String = "Vicente";
     mutable = "Adrian";
-
-
-
-
-
-
 
 
     //  val > var
@@ -35,21 +29,9 @@ fun main(){
     val fechaNacimiento: Date = Date()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
     // SWITCH
     val estadoCivilWhen = "C"
-    when (estadoCivilWhen){
+    when (estadoCivilWhen) {
         ("C") -> {
             println("Casado")
         }
@@ -66,11 +48,11 @@ fun main(){
     calcularSueldo(10.00)
     calcularSueldo(10.00, 15.00)
     calcularSueldo(10.00, 12.00, 20.00)
-    calcularSueldo(sueldo = 10.00,  tasa = 12.00, bonoEspecial = 20.00) //  Parametros nombrados
+    calcularSueldo(sueldo = 10.00, tasa = 12.00, bonoEspecial = 20.00) //  Parametros nombrados
     calcularSueldo(10.00, bonoEspecial = 20.00) // Named Parameters
     calcularSueldo(bonoEspecial = 20.00, sueldo = 10.00, tasa = 14.00) //  Parametros nombrados
 
-    val sumaUno = Suma(1,1)
+    val sumaUno = Suma(1, 1)
     val sumaDos = Suma(null, 1)
     val sumaTres = Suma(1, null)
     val sumaCuatro = Suma(null, null)
@@ -81,7 +63,6 @@ fun main(){
     println(Suma.pi)
     println(Suma.elevarAlCuadrado(2))
     println(Suma.historialSumas)
-
 
 
     // ARREGLOS
@@ -102,9 +83,6 @@ fun main(){
     println(arregloDinamico)
 
 
-
-
-
     // FOR EACH -> Unit
     // Iterar un arreglo
     val respuestaForEach: Unit = arregloDinamico
@@ -121,6 +99,85 @@ fun main(){
     println(respuestaForEach)
 
 
+
+
+
+
+
+
+
+    // MAP -> Muta el arreglo (Cambia el arreglo)
+    // 1) Enviemos el nuevo valor de la iteracion
+    // 2) Nos devuelve es un NUEVO ARREGLO con los
+    // valores modificados
+
+    val respuestaMap: List<Double> = arregloDinamico
+        .map { valorActual: Int ->
+            return@map valorActual.toDouble() + 100.00
+        }
+
+    println(respuestaMap)
+    val respuestaMapDos = arregloDinamico.map { it + 15 }
+
+
+
+
+    // OR AND
+    // OR ->  ANY (Alguno cumple?)
+    // AND ->  ALL (Todos cumplen?)
+
+    val respuestaAny: Boolean = arregloDinamico
+        .any { valorActual: Int ->
+            return@any (valorActual > 5)
+        }
+    println(respuestaAny) // true
+
+    val respuestaAll: Boolean = arregloDinamico
+        .all { valorActual: Int ->
+            return@all (valorActual > 5)
+        }
+    println(respuestaAll) // false
+
+
+    // REDUCE -> Valor acumulado
+    // Valor acumulado = 0 (Siempre 0 en lenguaje Kotlin)
+    // [1, 2, 3, 4, 5] -> Sumeme todos los valores del arreglo
+    // valorIteracion1 = valorEmpieza + 1 = 0 + 1 = 1 -> Iteracion 1
+    // valorIteracion2 = valorIteracion1 + 2 = 1 + 2 = 3 -> Iteracion 2
+    // valorIteracion3 = valorIteracion2 + 3 = 3 + 3 = 6 -> Iteracion 3
+    // valorIteracion4 = valorIteracion3 + 4 = 6 + 4 = 10 -> Iteracion 4
+    // valorIteracion5 = valorIteracion4 + 5 = 10 + 5 = 15 -> Iteracion 5
+    val respuestaReduce: Int = arregloDinamico
+        .reduce { // acumulado = 0 -> SIEMPRE EMPIEZA EN 0
+                acumulado: Int, valorActual: Int ->
+            return@reduce (acumulado + valorActual) // -> Logica negocio
+        }
+    println(respuestaReduce) // 78
+    // valorCarritoActual.cantidad *  valorCarritoActual.valor
+    // 2 x 195
+    // 1 x 10
+    // 1 x 10
+
+
+
+
+
+
+
+
+
+    // Filter -> FILTRAR EL ARREGLO
+    // 1) Devolver una expresion (TRUE o FALSE)
+    // 2) Nuevo arreglo filtrado
+    val respuestaFilter: List<Int> = arregloDinamico
+        .filter { valorActual: Int ->
+            // Expresion Condicion
+            val mayoresACinco: Boolean = valorActual > 5
+            return@filter mayoresACinco
+        }
+    val respuestaFilterDos = arregloDinamico.filter { it <= 5 }
+    println(respuestaFilter)
+    println(respuestaFilterDos)
 
 
 
