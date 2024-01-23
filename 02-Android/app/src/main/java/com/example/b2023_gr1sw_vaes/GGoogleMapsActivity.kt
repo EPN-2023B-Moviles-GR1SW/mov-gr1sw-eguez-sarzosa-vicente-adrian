d90c32fe004530a6d767030653205c71ad4c173b
Adrian Eguez <eadepto@hotmail.com>
2024-01-23 15:40:49 -0500

    Hasta finalizar mapas

--- 02-Android/app/src/main/java/com/example/b2023_gr1sw_vaes/GGoogleMapsActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023_gr1sw_vaes/GGoogleMapsActivity.kt
@@ -3,6 +3,8 @@ package com.example.b2023_gr1sw_vaes
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -13,6 +15,7 @@ import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolygonOptions
 import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.material.snackbar.Snackbar
 
 class GGoogleMapsActivity : AppCompatActivity() {
     private lateinit var mapa: GoogleMap
@@ -22,6 +25,26 @@ class GGoogleMapsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_ggoogle_maps)
         solicitarPermisos()
         iniciarLogicaMapa()
+        val boton = findViewById<Button>(R.id.btn_ir_carolina)
+        boton.
+        setOnClickListener {
+            irCarolina()
+        }
+    } // Finaliza el OnCreate!
+    fun irCarolina(){
+        val carolina = LatLng(-0.1825684318486696,
+            -78.48447277600916)
+        val zoom = 17f
+        moverCamaraConZoom(carolina, zoom)
+    }
+    fun mostrarSnackbar(texto: String) {
+        Snackbar
+            .make(
+                findViewById(R.id.cl_google_maps), // view
+                texto, // texto
+                Snackbar.LENGTH_LONG // tiempo
+            )
+            .show()
     }
 
     fun iniciarLogicaMapa() {
@@ -35,7 +58,32 @@ class GGoogleMapsActivity : AppCompatActivity() {
                 moverQuicentro()
                 anadirPolilinea()
                 anadirPoligono()
+                escucharListeners()
             }
+        }
+    }
+    fun escucharListeners(){
+        mapa.setOnPolygonClickListener {
+            mostrarSnackbar("setOnPolygonClickListener ${it.tag}")
+            it.tag // ID
+        }
+        mapa.setOnPolylineClickListener {
+            mostrarSnackbar("setOnPolylineClickListener ${it.tag}")
+            it.tag // ID
+        }
+        mapa.setOnMarkerClickListener {
+            mostrarSnackbar("setOnMarkerClickListener ${it.tag}")
+            it.tag // ID
+            return@setOnMarkerClickListener true
+        }
+        mapa.setOnCameraMoveListener {
+            Log.i("mapa", "setOnCameraMoveListener")
+        }
+        mapa.setOnCameraMoveStartedListener {
+            Log.i("mapa", "setOnCameraMoveStartedListener ${it}")
+        }
+        mapa.setOnCameraIdleListener {
+            Log.i("mapa", "setOnCameraIdleListener")
         }
     }
     fun anadirPolilinea(){
