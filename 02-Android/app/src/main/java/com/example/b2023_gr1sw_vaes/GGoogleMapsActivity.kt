52f3ffe7228f37679c5aab177b836cb9a6e5d018
Adrian Eguez <eadepto@hotmail.com>
2024-01-23 14:44:54 -0500

    poligono polillinea

--- 02-Android/app/src/main/java/com/example/b2023_gr1sw_vaes/GGoogleMapsActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023_gr1sw_vaes/GGoogleMapsActivity.kt
@@ -5,8 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolygonOptions
+import com.google.android.gms.maps.model.PolylineOptions
 
 class GGoogleMapsActivity : AppCompatActivity() {
     private lateinit var mapa: GoogleMap
@@ -26,8 +32,78 @@ class GGoogleMapsActivity : AppCompatActivity() {
             with(googleMap) {
                 mapa = googleMap
                 establecerConfiguracionMapa()
+                moverQuicentro()
+                anadirPolilinea()
+                anadirPoligono()
             }
         }
+    }
+    fun anadirPolilinea(){
+        with(mapa){
+            val poliLineaUno = mapa
+                .addPolyline(
+                    PolylineOptions()
+                        .clickable(true)
+                        .add(
+                            LatLng(-0.1759187040647396,
+                                -78.48506472421384),
+                            LatLng(-0.17632468492901104,
+                                -78.48265589308046),
+                            LatLng(-0.17746143130181483,
+                                -78.4770533307815)
+                        )
+                )
+            poliLineaUno.tag = "linea-1" // <- ID
+        }
+    }
+
+    fun anadirPoligono(){
+        with(mapa){
+            // POLIGONO
+            val poligonoUno = mapa
+                .addPolygon(
+                    PolygonOptions()
+                        .clickable(true)
+                        .add(
+                            LatLng(-0.1771546902239471,
+                                -78.48344981495214),
+                            LatLng(-0.17968981486125768,
+                                -78.48269198043828),
+                            LatLng(-0.17710958124147777,
+                                -78.48142892291516)
+                        )
+                )
+            poligonoUno.fillColor = -0xc771c4
+            poligonoUno.tag = "poligono-2" // <- ID
+        }
+    }
+
+    fun moverQuicentro(){
+        val zoom = 17f
+        val quicentro = LatLng(
+            -0.17556708490271092, -78.48014901143776
+        )
+        val titulo = "Quicentro"
+        val markQuicentro = anadirMarcador(
+            quicentro, titulo)
+        markQuicentro.tag = titulo
+        moverCamaraConZoom(quicentro, zoom)
+    }
+
+
+    fun anadirMarcador(latLng: LatLng, title: String): Marker {
+        return mapa.addMarker(
+            MarkerOptions()
+                .position(latLng)
+                .title(title)
+        )!!
+    }
+
+    fun moverCamaraConZoom(latLng: LatLng, zoom: Float = 10f){
+        mapa.moveCamera(
+            CameraUpdateFactory
+                .newLatLngZoom(latLng, zoom)
+        )
     }
 
     fun establecerConfiguracionMapa(){
